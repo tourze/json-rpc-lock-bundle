31f0be39a9089c1f47673e3fbd9e1ed5539ddea9
tourze-ci <you@example.com>
2025-04-14 08:47:46 +0000

    feat(json-rpc-lock-bundle): 添加JsonRPC锁定处理插件

--- README.md
+++
@@ -1,1 +0,0 @@
-# json-rpc-lock-bundle
---
+++ src/JsonRPCLockBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Tourze\JsonRPCLockBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class JsonRPCLockBundle extends Bundle
+{
+}
