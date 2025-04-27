5e3c3c7cf21448543325b627ea4105ac5c79b8a5
github-ci <you@example.com>
2025-04-27 16:54:29 +0000

    忽略无用调用堆栈+

--- src/JsonRPCLockBundle.php
+++ src/JsonRPCLockBundle.php
@@ -3,7 +3,14 @@
 namespace Tourze\JsonRPCLockBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BacktraceHelper\Backtrace;
+use Tourze\JsonRPCLockBundle\Procedure\LockableProcedure;
 
 class JsonRPCLockBundle extends Bundle
 {
+    public function boot(): void
+    {
+        parent::boot();
+        Backtrace::addProdIgnoreFiles((new \ReflectionClass(LockableProcedure::class))->getFileName());
+    }
 }
